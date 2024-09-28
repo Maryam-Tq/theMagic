8ca024ed950742c523ef2c9701cb370382b1e353
Maryam <maryamtariqbiz@gmail.com>
2024-09-28 04:03:31 -0700

    Add files via upload

---
+++ TheMagic.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2;
+    
+    // Prompt the user to enter two values
+    cout << "Enter the first value: ";
+    cin >> num1;
+    
+    cout << "Enter the second value: ";
+    cin >> num2;
+
+    // Perform an operation to ensure the result is always 11
+    // Here, we'll use the formula: 11 = num1 + num2 - (num1 + num2 - 11)
+    int result = num1 + num2 - (num1 + num2 - 11);
+
+    // Output the result
+    cout << "The result is: " << result << endl;
+
+    return 0;
+}
+
